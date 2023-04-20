79725942488daa88d9d2c9bf09079b441a001280
Jonathan Good <github@bungogood.com>
2023-04-20 20:46:18 +0100

    Adding next auth using GitHub

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import { Analytics } from '@vercel/analytics/react';
+
+import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
-    <>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </SessionProvider>
   );
 };
 
